0f80c3cd88a0399bc7b9e95fa8ee4bb675e2d00d
Jamal khan Jamal <jamalkhanmrd151@gmial.com>
2026-03-21 15:46:43 +0500

    New file(list) added

---
+++ harry_python/list.py
@@ -0,0 +1,50 @@
+marks=[5,6,8,3,4,5,"harry"]
+# print(marks)
+# print(marks[0])
+# print(type(marks))
+
+# negative index
+print(marks[-3])
+print(marks[len(marks)-3])
+print(6-3)
+print(marks[3])
+
+
+
+
+# finding in list 
+if 5 in marks:
+    print("yes")
+else:
+    print("no")
+
+
+if "arry" in "harry":
+    print("yes")
+
+
+print(marks)
+print(marks[:])
+print(marks[1:-1])        # nagative indexing technique using
+print(marks[1:6]) # same as above 
+# jump index
+print(marks[1:4:2])
+
+
+
+
+
+
+
+
+
+
+
+
+# list comprehension
+lst=[i for i in range(4)]
+print(lst)
+# list comprehension
+lst=[i for i in range(10) if i%2==0]
+print(lst)
+
